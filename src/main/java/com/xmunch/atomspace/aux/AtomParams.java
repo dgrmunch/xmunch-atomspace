8e88c69db8c35e563c05155b89cc01f6473e061c
Diego González Rodríguez <xmunch@xmunch.com>
2014-12-08 22:42:31 +0100

    Add AtomParams

---
+++ src/main/java/com/xmunch/atomspace/aux/AtomParams.java
@@ -0,0 +1,61 @@
+package com.xmunch.atomspace.aux;
+
+public enum AtomParams {
+
+	VERTEX {
+		@Override
+		public String get() {
+			return "vertex";
+		}
+	},
+	EDGE {
+		@Override
+		public String get() {
+			return "edge";
+		}
+	},
+	STRENGTH {
+		@Override
+		public String get() {
+			return "strength";
+		}
+	},
+	FROM {
+		@Override
+		public String get() {
+			return "from";
+		}
+	},
+	TO {
+		@Override
+		public String get() {
+			return "to";
+		}
+	}, 
+	VERTEX_TYPE{
+		@Override
+		public String get() {
+			return "vertex_type";
+		}
+	},
+	VERTEX_LABEL{
+		@Override
+		public String get() {
+			return "vertex_label";
+		}
+	}, 
+	EDGE_TYPE{
+		@Override
+		public String get() {
+			return "edge_type";
+		}
+	},
+	EDGE_LABEL{
+		@Override
+		public String get() {
+			return "edge_label";
+		}
+	};
+
+	public abstract String get();
+}
