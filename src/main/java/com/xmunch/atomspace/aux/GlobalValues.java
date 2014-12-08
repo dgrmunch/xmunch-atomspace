d12954c919a34774f92ad64a948d86ac97abb4d5
Diego González Rodríguez <xmunch@xmunch.com>
2014-12-08 23:31:45 +0100

    Add colors to GlobalValues

--- src/main/java/com/xmunch/atomspace/aux/GlobalValues.java
+++ src/main/java/com/xmunch/atomspace/aux/GlobalValues.java
@@ -19,6 +19,42 @@ public enum GlobalValues {
 		public String get() {
 			return "1";
 		}
+	},
+	RED {
+		@Override
+		public String get() {
+			return "#FF3300";
+		}
+	},
+	BLUE {
+		@Override
+		public String get() {
+			return "#00CCFF";
+		}
+	},
+	GREEN {
+		@Override
+		public String get() {
+			return "#00FF00";
+		}
+	},
+	YELLOW {
+		@Override
+		public String get() {
+			return "#FFFF00";
+		}
+	},
+	PINK {
+		@Override
+		public String get() {
+			return "#FF00FF";
+		}
+	},
+	WHITE {
+		@Override
+		public String get() {
+			return "#FFFFFF";
+		}
 	};
 
 	public abstract String get();
